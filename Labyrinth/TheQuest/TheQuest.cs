dd1a192cbdae34ea61ba0f7ba71636114e40e46c
Teodor Popov <teodor.popoff@gmail.com>
2015-02-20 23:43:46 +0200

    Console scrollbar and Items Position
    TheQuest.cs: remove the scrollbar
    Item.cs: added random number for position of the items

--- Labyrinth/TheQuest/TheQuest.cs
+++ Labyrinth/TheQuest/TheQuest.cs
@@ -11,6 +11,9 @@ namespace TheQuest
             GameEngine engine = new GameEngine(keyboard, renderer);
             Dwarf mainChar = new Dwarf("asd", new Location(1, 1));
 
+
+            Console.BufferHeight = Console.WindowHeight; // Remove the scrollbar
+
             keyboard.OnDownPressed += (sender, eventInfo) => { mainChar.Move(Direction.Down); };
             keyboard.OnUpPressed += (sender, eventInfo) => { mainChar.Move(Direction.Up); };
             keyboard.OnLeftPressed += (sender, eventInfo) => { mainChar.Move(Direction.Left); };
