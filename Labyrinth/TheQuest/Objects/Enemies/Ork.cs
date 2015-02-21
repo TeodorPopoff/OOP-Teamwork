e29c86ea2bb2288eab7c2b16b80bf3c277fc0bcb
Teodor Popov <teodor.popoff@gmail.com>
2015-02-22 01:43:38 +0200

    Ork, Troll, Warg

--- Labyrinth/TheQuest/Objects/Enemies/Ork.cs
+++ Labyrinth/TheQuest/Objects/Enemies/Ork.cs
@@ -2,7 +2,51 @@
 
 namespace TheQuest
 {
-    public class Ork 
+    public class Ork : Character, IEnemy
     {
+        private int battleStrength = 60;
+        private bool isAlive = true;
+
+        public Ork(string name, Location position)
+            : base(name, position)
+        {
+            base.symbol = "u+0046";
+            base.description = "Orcs were the most commonplace villains serving the Dark Powers, a race of sentient beings bred by the evil Vala Melkor during the time of the Great Darkness. The Dark Lord Sauron, and later the wizard Saruman, also bred them and used them as soldiers and henchmen to do various evil deeds across Middle-earth.";
+        }
+
+        public override bool IsAlive
+        {
+            get
+            {
+                return this.IsAlive;
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return base.description;
+            }
+        }
+
+        public override int BattleStrength
+        {
+            get
+            {
+                return this.battleStrength;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    this.isAlive = false;
+                }
+                this.battleStrength = value;
+            }
+        }
     }
+
+
 }
--- Labyrinth/TheQuest/Objects/Enemies/Troll.cs
+++ Labyrinth/TheQuest/Objects/Enemies/Troll.cs
@@ -2,8 +2,49 @@
 
 namespace TheQuest
 {
-    public class Troll 
+    public class Troll : Character, IEnemy
     {
+        private int battleStrength = 40;
+        private bool isAlive = true;
 
+        public Troll(string name, Location position)
+            : base(name, position)
+        {
+            base.symbol = "u+0047";
+            base.description = "Trolls are a very large and monstrous (ranging from between 8 to 10 feet tall), and for the most part unintelligent (references are made about more cunning trolls[1]) humanoid race inhabiting Middle-earth.";
+        }
+
+        public override bool IsAlive
+        {
+            get
+            {
+                return this.IsAlive;
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return base.description;
+            }
+        }
+
+        public override int BattleStrength
+        {
+            get
+            {
+                return this.battleStrength;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    this.isAlive = false;
+                }
+                this.battleStrength = value;
+            }
+        }
     }
 }
