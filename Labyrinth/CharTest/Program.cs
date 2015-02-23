0d40d86a48990d17117ba7b7d34b598de698eebe
straho99 <strahil.ruychev@gmail.com>
2015-02-23 11:39:15 +0200

    Unicode tests

---
+++ Labyrinth/CharTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Ĥėļŀō ŵŏŗłđ");
+
+            //Console.OutputEncoding = System.Text.Encoding.Unicode;
+            byte[] testUnicode = new byte[] { 240, 159, 145, 186 };
+            string unicodeSymbol = Encoding.UTF8.GetString(testUnicode);
+            Console.WriteLine(unicodeSymbol);
+            
+        }
+    }
+}
