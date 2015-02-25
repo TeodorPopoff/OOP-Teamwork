596cbb73e87e30ff914a6c8299185eb3d3fe25d4
straho99 <strahil.ruychev@gmail.com>
2015-02-25 12:02:04 +0200

    legolas

---
+++ Labyrinth/TheQuest/Objects/Elves/Legolas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TheQuest
+{
+    public sealed class Legolas : Elf
+    {
+        private bool _isAlive = true;
+        private static Location _position;
+        private static Legolas hero;
+
+        public static Legolas GetInstance(Location position)
+        {
+            if (Legolas.hero == null)
+            {
+                Legolas.hero = new Legolas(position);
+                return Legolas.hero;
+            }
+            else
+            {
+                return Legolas.hero;
+            }
+        }
+
+        private Legolas(Location position)
+            : base("Legolas", position)
+        {
+            Random rnd = new Random();
+            base.symbol = "Le";
+            base.description = "He is the son of the Elf-king Thranduil of Mirkwood, a Prince of the Woodland Realm (Mirkwood), a messenger, and a master bowman.";
+            base.BattleStrength = 170;
+        }
+    }
+}
